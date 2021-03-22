0a391aced0f3c765ce0d7fbc7b1190cd9dde25e2
Maria <m.tarliuk@mail.ru>
2021-03-22 12:27:26 +0300

    Fix nav styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,20 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Image, Nav, Navbar} from "react-bootstrap"
 import {commonIcons} from "../resources/images"
 import {Link} from "gatsby";
+import '../styles/header.css';
 
 const linksArray = ["#learnMore", "#services", "#partners", "#ourWork",
     "/startups", "/investment", "/blog"]
 
-const headerStyle = {
-        position: 'relative',
-        zIndex: 100
-}
-
-const navBlock = {
-    position: 'absolute',
-    left: '0%',
-    top: '47%',
-    width: '100%',
-    zIndex: 200
-}
-
 const navXs = 'padding-left: 15px; position: absolute; left: 0%; top: 110%; width: 100%; z-index: 200; background: rgba(255, 255, 255, 0.6);'
-
-function clickToggle() {
-    if (!document.querySelector('.navbar-collapse').classList.contains('show')) {
-       if (document.querySelector('.mainContainer')) document.querySelector('.mainContainer').setAttribute('style','filter: blur(4px);');
-       document.querySelector('#responsive-navbar-nav').setAttribute('style', navXs);
-    } else {
-        document.querySelector('.mainContainer').setAttribute('style', '');
-    }
-}
-
-function changeBgBlur() {
-    if(document.querySelector('.mainContainer')) {
-        document.querySelector('.mainContainer').setAttribute('style', '');
-    }
-}
 
 const NavLinks = () => {
     return (
         linksArray.map((elem, index) => {
             return (<Link key={elem + index} to={elem} style={{textDecoration: "none"}} >
-                    <Nav.Link href={elem} onClick={changeBgBlur}>
+                    <Nav.Link href={elem} 
+                    >
                         {elem.slice(1).charAt(0).toUpperCase() + elem.slice(2).replace(/([A-Z])/g, ' $1').trim()}
                     </Nav.Link></Link>
             )
@@ -49,22 +23,46 @@ const NavLinks = () => {
 }
 
 const Header = () => {
+    const navEl = useRef();
+    const blurBg = useRef();
+    const onButtonClick = () => {
+        if (!navEl.current.classList.contains('show')) {
+            navEl.current.setAttribute('style', navXs);
+            blurBg.current.classList.add('blur');
+        } else {
+            blurBg.current.classList.remove('blur');
+        }
+  };
+
+    useEffect(() => {
+        window.onresize = () => {
+            if  (document.documentElement.clientWidth >= 991) {
+                navEl.current.setAttribute('style', '');
+                blurBg.current.classList.remove('blur');
+            } else {
+                navEl.current.setAttribute('style', navXs);
+                if (navEl.current.classList.contains('show')) blurBg.current.classList.add('blur');
+            }
+        }
+    })
+
     return (
-        <Navbar collapseOnSelect expand="lg" className="container mt-3 mb-3" style={headerStyle}>
+        <Navbar collapseOnSelect expand="lg" className="container mt-3 mb-3" className='headerStyle'>
             <Navbar.Brand
                 className="justify-content-start">
                 <Link to="/">
                     <Image src={commonIcons.remedyIcon}/>
                 </Link>
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={clickToggle}/>
-            <Navbar.Collapse id="responsive-navbar-nav" style={navBlock}>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={onButtonClick}/>
+            <Navbar.Collapse id="responsive-navbar-nav" ref={navEl} className='navBlock'>
                 <Nav className="mr-auto">
                 </Nav>
                 <Nav className="justify-content-end">
                     <NavLinks/>
                 </Nav>
             </Navbar.Collapse>
+            <div className="transparentBg" ref={blurBg}></div>
         </Navbar>
     )
 }
