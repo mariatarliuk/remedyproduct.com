e574f9f8ec219b3653e1efbd27354e3707d287a2
Kvinto <Kvinto1986@gmail.com>
2021-04-02 16:30:57 +0300

    fix routing

--- src/pages/investment.js
+++ src/pages/investment.js
@@ -2,13 +2,19 @@ import * as React from "react";
 import MainPageTemplate from "../templates/MainPageTemplate"
 import "../styles/index.css";
 import InvestmentPageContent from '../components/pagesContent/InvestmentPageContent'
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
 
-const IndexPage = () => {
+const IndexPage = ({location}) => {
     return (
         <>
+            <Header
+                path={location.pathname}
+            />
             <MainPageTemplate>
                 <InvestmentPageContent />
             </MainPageTemplate>
+            <Footer/>
         </>
     )
 }
